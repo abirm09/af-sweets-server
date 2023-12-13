ec1c4d21c7f8177f1a4f5f705ec764785cea9f81
Md. Abir mahmud <abirmahmud5665@gmail.com>
2023-12-13 15:29:32 +0600

    done project setup

---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const env = process.env;
+
+export default {
+  ENV: env.NODE_ENV,
+  PORT: env.PORT,
+  DB_URL:
+    env.NODE_ENV === "production"
+      ? env.DB_URL_PRODUCTION
+      : env.DB_URL_DEVELOPMENT,
+  CLIENT_SIDE_URL: env.CLIENT_SIDE_URL,
+  CLIENT_SIDE_HOST: env.CLIENT_SIDE_HOST,
+  ACCESS_TOKEN_SECRET: env.ACCESS_TOKEN_SECRET,
+  GOOGLE_SMTP_USER: env.GOOGLE_SMTP_USER,
+  GOOGLE_SMTP_PASSWORD: env.GOOGLE_SMTP_PASSWORD,
+};
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+const bootstrap = async () => {
+  try {
+    await mongoose.connect(config.DB_URL as string);
+    app.listen(config.PORT, () => {
+      console.log(
+        `server is started at ${
+          config.ENV === "development"
+            ? "http://localhost:"
+            : "production and port "
+        }${config.PORT}`
+      );
+    });
+    console.log(`Database is connected at ${config.DB_URL}`);
+  } catch (error) {
+    console.log(`Failed to connect to database.`, error);
+  }
+};
+
+bootstrap();
